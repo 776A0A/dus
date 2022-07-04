550675570ad0c7f4567ad1a4be2e3f0089e131e1
王捷 <har3dy@icloud.com>
2022-07-05 01:09:15 +0800

    feat(playground): 调整导航页样式

--- playground/src/views/index.vue
+++ playground/src/views/index.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="flex-col fullscreen center-flex">
-    <div class="border rounded">
-      <h1 class="text-lg mb-4">目录</h1>
+    <div class="bg-white border rounded">
+      <h1 class="border-b text-lg py-2 px-4">目录</h1>
       <ul
         class="list-circle text-base children:(transition-all list-none m-0 py-2 px-4 hover-shadow cursor-pointer) "
       >
@@ -19,6 +19,10 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <style scoped>
+nav {
+  background-image: url('assets/pexels-skyler-ewing-4793432.jpg');
+  @apply bg-no-repeat bg-cover bg-center;
+}
 a {
   color: inherit;
 }
