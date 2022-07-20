94de8d726e00edd2e142eb3a55dc75d05ceedd9a
王捷 <har3dy@icloud.com>
2022-07-21 04:25:04 +0800

    feat(tools): convertNames可递归替换属性名

--- packages/tools/src/convertNames.ts
+++ packages/tools/src/convertNames.ts
@@ -5,18 +5,19 @@ type ConvertObjName = <
   }
 >(
   originalObj: T,
-  namesMap: Readonly<R>
+  namesMap: Readonly<R>,
+  options?: Partial<{ deep: boolean; childKey: string }>
 ) => RecordWithSwappedKey<T, R>
 
 /**
  * 转换对象的名称
  * @param obj - 源对象
  * @param namesMap - 目标key的对象
+ * @param options - 可传入 deep 用于递归处理子对象，默认 { deep: false, childKey: 'children' }
  * @returns 替换后的对象，源对象中被替换的key将会删除，同时保留类型
  */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export const convertNames: ConvertObjName = (obj, namesMap) => {
+export const convertNames: ConvertObjName = (obj, namesMap, options) => {
   if (!obj || typeof obj !== 'object') {
     return obj
   }
@@ -28,6 +29,17 @@ export const convertNames: ConvertObjName = (obj, namesMap) => {
       convertedObj[namesMap[key as keyof typeof namesMap]] = obj[key]
     } else {
       convertedObj[key] = obj[key]
+    }
+
+    if (options?.deep) {
+      const key = options.childKey || 'children'
+
+      if (Array.isArray(convertedObj[key])) {
+        const children = [...convertedObj[key]]
+        convertedObj[key] = children.map((child) =>
+          convertNames(child, namesMap, options)
+        )
+      }
     }
   })
 
