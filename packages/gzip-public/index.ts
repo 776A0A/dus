b4f571d65aa0cdde599c6155f0da1548e602e37d
王捷 <har3dy@icloud.com>
2022-07-22 10:06:12 +0800

    feat(gzip-public): 检查public是否存在，不存在直接跳过

--- packages/gzip-public/index.ts
+++ packages/gzip-public/index.ts
@@ -12,6 +12,17 @@ compress(publicPath)
 
 async function compress(dirPath: string) {
   try {
+    let exist = false
+
+    try {
+      fs.accessSync(dirPath)
+      exist = true
+    } catch (error) {
+      exist = false
+    }
+
+    if (!exist) return
+
     const files = fs.readdirSync(dirPath)
     const absPathOfFiles = files.map((file) => resolve(`${dirPath}/${file}`))
 
