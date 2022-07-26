5f4e78516e0eca22f611040b9c3836f877433cbb
王捷 <har3dy@icloud.com>
2022-07-26 14:04:27 +0800

    feat(tools): 新增isBoolean, isFunction，删除texts

--- packages/tools/src/type.ts
+++ packages/tools/src/type.ts
@@ -14,3 +14,11 @@ export function isNullable(value: unknown): value is null | undefined {
 export function isObject(value: unknown): value is object {
   return Object.prototype.toString.call(value) === '[object Object]'
 }
+
+export function isBoolean(value: unknown): value is boolean {
+  return typeof value === 'boolean'
+}
+
+export function isFunction(value: unknown): value is Function {
+  return typeof value === 'function'
+}
